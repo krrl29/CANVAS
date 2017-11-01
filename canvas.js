9006b8a34b9134a6a99b38cb08be7754505550f8
Your Name <you@example.com>
2017-11-01 11:35:51 -0400

    actually fixed

--- canvas.js
+++ canvas.js
@@ -5,12 +5,12 @@ var cvsRect;
 window.onload = function () {
     //Canvas contexts
     canvasSetup();
-    //ref to drawCirc
-    //$("cvsCirc").addEventListener("click", drawCirc());
-    //ref to drawRect
-    //$("cvsRect").addEventListener("click", drawRect());
     //listeners for button events
-    $("btnRect").addEventListener("click", drawRect());//.onclick = drawRect();
+    $("btnRect").addEventListener("click", drawRect); 
+    $("btnCirc").addEventListener("click", drawCirc);
+    //listeners for clear events
+    $("cvsCirc").addEventListener("click", clearCirc);
+    $("cvsRect").addEventListener("click", clearRect);
 
     //test
     //cvsRect.beginPath();
@@ -27,51 +27,62 @@ function $(id) {
 }
 //function that draws circles
 function drawCirc() {
-    //use context of the canvas
-    ctxCirc.beginPath();
-    //prompt user to enter a number and store n
+    //set up drawing context
+    var ctxCirc = cvsCirc.getContext("2d");
+
+    //grab number from text box and store in n
     var n = $("txtCirc").value;
+    //grab size of canvas for location and size calculations
+    var cvsWidth = $("cvsCirc").width;
+    var cvsHeight = $("cvsCirc").height;
     //loop to draw circles n times
     for (i = 0; i < n, i++;) {
         //randomize size r- radius
 
         //random location on canvas (x,y)
-
+        var w = Math.random() * (cvsWidth / 4);
+        var h = Math.random() * (cvsHeight / 4);
 
         //randomize color and opacity
         ctxCirc.fillStyle = rdmColor();
+
+    
 
 
     }
 }
 //function that draws rectangles
 function drawRect() {
-    
-    var ctxRect = cvsRect.getContext("2d");//set up drawing context    
-    //prompt user to enter a number and store n
+
+    //set up drawing context    
+    var ctxRect = cvsRect.getContext("2d");
+
+    //grab number from text box and store in n
     var n = $("txtRect").value;
+
     //grab size of canvas for location and size calculations
-    //var cvsWidth = $("cvsRect").width;
-    //var cvsHeight = $("cvsRect").height;
+    var cvsWidth = $("cvsRect").width;
+    var cvsHeight = $("cvsRect").height;
+
     //loop to draw boxes n times
-    for (i = 0; i < 5, i++;) {
+    for (i = 0; i < n; i++) {
         //randomize size
-        var x = Math.random() * .7//(cvsWidth - 10);
-        var y = Math.random() * .7//(cvsHeight - 10);
+        var x = Math.random() * (cvsWidth - 10);
+        var y = Math.random() * (cvsHeight - 10);
+
         //random location on canvas (x,y)
-        var w = Math.random() * .7//(cvsWidth / 4);
-        var h = Math.random() * .7//(cvsHeight / 4);
-        //randomize color and opacity
-        //ctxRect.fillStyle = rdmColor();
-        var r = Math.floor(Math.random() * 256); // 0-255
-        var g = Math.floor(Math.random() * 256); // 0-255
-        var b = Math.floor(Math.random() * 256); // 0-255
-        var a = Math.random() + .2;
+        var w = Math.random() * (cvsWidth / 4);
+        var h = Math.random() * (cvsHeight / 4);
+
         //create rectangle
         ctxRect.beginPath();
-        ctxRect.Rect(x, y, w, h);
-        ctxRext.Stroke();
-        ctxRect.fillStyle ='rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
+        ctxRect.rect(x, y, w, h);
+        //create border
+        ctxRect.stroke();
+        //randomize color
+        ctxRect.fillStyle = rdmColor();
+        //fill rectangle
+        ctxRect.fill();
 
     }
 
@@ -91,8 +102,6 @@ function rdmColor() {
     var g = Math.floor(Math.random() * 256); // 0-255
     var b = Math.floor(Math.random() * 256); // 0-255
     var a = Math.random();
-    return "'rgba(' + r + ',' + g + ',' + b + ',' + a + ')'";
+    var out = 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
+    return out;
 }
-
-
-
